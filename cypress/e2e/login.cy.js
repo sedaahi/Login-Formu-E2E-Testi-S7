b5ee388ddb6c41d25642b476ce4099a02fdf51b3
Seda Ahi <92363990+sedaahi@users.noreply.github.com>
2026-04-02 16:11:43 +0300

    cypress test düzeltmeleri*

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -12,39 +12,36 @@ describe("Login Form E2E", () => {
     cy.get('[data-cy="submit-button"]').click();
 
     cy.url().should("include", "/success");
-    cy.get('[data-cy="success-message"]').should("be.visible");
-    cy.contains("Başarıyla giriş yaptınız.").should("be.visible");
   });
 
-  it("email yanlışsa 1 hata mesajı gösterir ve buton disabled kalır", () => {
+  it("email yanlışsa 1 hata mesajı gösterir, doğru hata mesajı vardır ve buton disabled kalır", () => {
     cy.get('[data-cy="email-input"]').type("yanlis");
     cy.get('[data-cy="password-input"]').type("Abcd1234");
     cy.get('[data-cy="terms-input"]').check();
 
+    cy.get('[data-cy$="-error"]').should("have.length", 1);
     cy.get('[data-cy="email-error"]')
       .should("be.visible")
       .and("contain", "Geçerli bir email giriniz.");
-
     cy.get('[data-cy="submit-button"]').should("be.disabled");
   });
 
-  it("email ve password yanlışsa 2 hata mesajı gösterir", () => {
+  it("email ve password yanlışsa 2 hata mesajı gösterir, password hata mesajı vardır ve buton disabled kalır", () => {
     cy.get('[data-cy="email-input"]').type("yanlis");
     cy.get('[data-cy="password-input"]').type("1234");
     cy.get('[data-cy="terms-input"]').check();
 
-    cy.get('[data-cy="email-error"]').should("be.visible");
+    cy.get('[data-cy$="-error"]').should("have.length", 2);
     cy.get('[data-cy="password-error"]')
       .should("be.visible")
       .and(
         "contain",
         "Şifre en az 8 karakter olmalı, bir büyük harf, bir küçük harf ve bir rakam içermelidir."
       );
-
     cy.get('[data-cy="submit-button"]').should("be.disabled");
   });
 
-  it("email ve password doğru ama şartlar kabul edilmediyse buton disabled kalır", () => {
+  it("email ve password doğru ama kurallar kabul edilmediyse buton disabled kalır", () => {
     cy.get('[data-cy="email-input"]').type("test@test.com");
     cy.get('[data-cy="password-input"]').type("Abcd1234");
 
