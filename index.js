d0b689933c35fb6a9b92c00dbacb91c1ed82c64a
Seda Ahi <92363990+sedaahi@users.noreply.github.com>
2026-04-02 15:55:06 +0300

    cypress yüklendi

---
+++ index.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+function cypressTestProjesi() {
+  const checklist = {
+    project_url: 'https://github.com/kullanici-adi/proje-adi',
+  };
+
+  return checklist;
+}
+
+/* Bu satırdan sonraki satırları silmeyin! */
+module.exports = cypressTestProjesi;
